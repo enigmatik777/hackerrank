3337e28c82d59c91d484a31105f124b78e047472
enigmatik777 <denis.budeacenco@hotmail.com>
2018-07-26 10:02:55 +0200

    Hackerrank

---
+++ python/cr_disp_var.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2.7
+first_name = raw_input('Entet your First Name:')
+last_name = raw_input('Entet your Last Name:')
+age = int(raw_input("Enter your age:"))
+birth_date = raw_input("Enter birthday:")
+print ("My name is %s %s " % (first_name,last_name))
+print ("I was born on %s and I'm %s years old" % (birth_date,age))
