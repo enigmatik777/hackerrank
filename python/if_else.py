7aa9061ea869e0d9c4b39056a5f1a4c7a2b35509
enigmatik777 <denis.budeacenco@hotmail.com>
2018-07-26 10:39:39 +0200

    New script iterate

---
+++ python/if_else.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+user = { 'admins': True, 'active': True, 'name': 'Kevin' }
+if user['admin'] and user['active']:
+   print("ACTIVE - (ADMIN) %s" % user['name'])
+elif user['admin']:
+   print("(ADMIN) %s" % user['name'])
+elif user['active']:
+   print("ACTIVE - %s" % user['name'])
+else:
+   print(user['name'])
+
